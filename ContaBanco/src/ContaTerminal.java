d41ac3d3180ff71dfdd3b19ed23d094294697f8c
Mateus3007 <mateus3007araujo@gmail.com>
2023-02-18 03:12:22 -0300

    finalização do simulador

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("informe o seu nome");
+        int numero = scan.nextInt();
+        System.out.println("informe o seu nome");
+        String agencia = scan.nextLine();
+        System.out.println("informe o seu nome");
+        String nome = scan.nextLine();
+        System.out.println("informe o seu nome");
+        double saldo = scan.nextDouble();
+        
     }
+
+   
+
+    
+    
 }
