6195f0a759a1ec4bd67b60fbdde349b138ab851e
Mateus3007 <mateus3007araujo@gmail.com>
2023-02-18 03:12:35 -0300

    .

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,15 +4,16 @@ public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        System.out.println("informe o seu nome");
+        System.out.println("informe o seu numero");
         int numero = scan.nextInt();
-        System.out.println("informe o seu nome");
+        System.out.println("informe o sua agencia");
+        String vazio = scan.nextLine();
         String agencia = scan.nextLine();
         System.out.println("informe o seu nome");
         String nome = scan.nextLine();
-        System.out.println("informe o seu nome");
+        System.out.println("informe o seu saldo");
         double saldo = scan.nextDouble();
-        
+        System.out.println("Olá "+ nome+ ", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
     }
 
    
